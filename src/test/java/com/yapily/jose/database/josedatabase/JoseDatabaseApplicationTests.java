9695ecbf5075ae6fe3218208804ee56278200daf
Quentin Castel <quentin.castel@yapily.com>
2020-06-16 11:02:50 +0100

    Init repo

---
+++ src/test/java/com/yapily/jose/database/josedatabase/JoseDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yapily.jose.database.josedatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoseDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
